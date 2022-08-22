e608082fe4e0afad89eadcea986f529bf1218de6
YOUSEF ALSARRAJ <yousef.edu@outlook.com>
2022-08-22 13:02:59 +0300

    Update Parts.js

--- src/components/Education/Parts.js
+++ src/components/Education/Parts.js
@@ -11,7 +11,7 @@ export default function Parts() {
           
           <div className='uni-one card-img'></div>
           <h1>Ankara University</h1>
-          <p className='text-al'>Computer science faculty with a 4 year batchelor degree, this is where my Computer software engineering life starts .</p>
+          <p className='text-al'>Computer science faculty with a 4 year bachelor degree, this is where my Computer software engineering life starts .</p>
           <a href='https://www.ankara.edu.tr/' rel="noreferrer" target="_blank" >University website</a>
         </div>
       </div>
@@ -21,7 +21,7 @@ export default function Parts() {
         <div className="content">
           <div className='uni-2 card-img'></div>
           <h1>University of Castilla-La Mancha</h1>
-          <p className='text-al'>I was here as an Exchange Student when I participated in the Erasmus Program , I practiced my how to deploy and make projects from scratch here</p>
+          <p className='text-al'>I was here as an Exchange Student when I participated in the Erasmus Program , I practiced how to deploy and make projects from scratch here</p>
           <a href='https://www.uclm.es/?sc_lang=en' rel="noreferrer" target="_blank">University website</a>
         </div>
       </div>
